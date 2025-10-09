84af8125a9b7142e5f06146dbf519182c1f92b38
CaPlanePourNous <caplanepournous@etik.com>
2025-10-09 22:05:20 +0200

    Update page.tsx

--- app/display/lottery/[slug]/page.tsx
+++ app/display/lottery/[slug]/page.tsx
@@ -1,8 +1,14 @@
+// app/display/lottery/[slug]/page.tsx
 import LotteryDisplay from "./screen";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
-export default function Page({ params }: { params: { slug: string } }) {
-  return <LotteryDisplay slug={params.slug} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  return <LotteryDisplay slug={slug} />;
 }
